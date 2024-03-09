e3cc0550bb7af78a2051d2846ad36f3bf812582e
GDWR <gregory.dwr@gmail.com>
2024-03-09 18:13:43 +0000

    Add Exit command

--- crates/knowsql_parser/src/lib.rs
+++ crates/knowsql_parser/src/lib.rs
@@ -4,6 +4,7 @@ use nom::{branch::alt, bytes::complete::tag, IResult};
 pub enum Command<'a> {
     Get(&'a str),
     Set(&'a str, &'a str),
+    Exit,
 }
 
 fn parse_set(input: &str) -> IResult<&str, Command> {
@@ -20,8 +21,13 @@ fn parse_get(input: &str) -> IResult<&str, Command> {
     Ok((input, Command::Get(key)))
 }
 
+fn parse_exit(input: &str) -> IResult<&str, Command> {
+    let (input, _) = tag("exit")(input)?;
+    Ok((input, Command::Exit))
+}
+
 pub fn parse_command(input: &str) -> Option<Command> {
-    match alt((parse_get, parse_set))(input) {
+    match alt((parse_get, parse_set, parse_exit))(input) {
         Ok((_, command)) => Some(command),
         _ => None,
     }
