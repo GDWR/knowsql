c401d6efec1aaba2f3c45091901822a14695fc31
GDWR <gregory.dwr@gmail.com>
2024-03-10 19:21:35 +0000

    nix fmt

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, ... }:
+{ pkgs ? import <nixpkgs> { }, ... }:
 pkgs.mkShell {
   RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
   packages = with pkgs; [ rustc cargo clippy rustfmt rust-analyzer mdbook ];
