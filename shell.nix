845bda7f3cadca163d2dbd29487169b2beb0d1ec
GDWR <gregory.dwr@gmail.com>
2024-03-10 17:23:59 +0000

    Add mdbook

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 pkgs.mkShell {
   RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-  packages = with pkgs; [ rustc cargo clippy rustfmt rust-analyzer ];
+  packages = with pkgs; [ rustc cargo clippy rustfmt rust-analyzer mdbook ];
 }
