d2f9f68dc83fde53c270cdce87033c67cf104041
GDWR <gregory.dwr@gmail.com>
2024-03-24 12:23:12 +0000

    Refactor BulkString to remove length

--- crates/knowsql_parser/src/resp2.rs
+++ crates/knowsql_parser/src/resp2.rs
@@ -11,26 +11,24 @@ pub fn parse_command(input: &[u8]) -> IResult<&[u8], Command> {
 
     if let resp2::Data::Array(arr) = data {
         match arr[..] {
-            [resp2::Data::BulkString {
-                data: "COMMAND", ..
-            }, resp2::Data::BulkString { data: "DOCS", .. }] => {
+            [resp2::Data::BulkString("COMMAND"), resp2::Data::BulkString("DOCS")] => {
                 Ok((remaining, Command::Command(SubCommand::Docs)))
             }
-            [resp2::Data::BulkString { data: "DBSIZE", .. }] => Ok((remaining, Command::DbSize)),
-            [resp2::Data::BulkString { data: "ECHO", .. }, resp2::Data::BulkString { data, .. }] => {
+            [resp2::Data::BulkString("DBSIZE")] => Ok((remaining, Command::DbSize)),
+            [resp2::Data::BulkString("ECHO"), resp2::Data::BulkString(data)] => {
                 Ok((remaining, Command::Echo(data)))
             }
-            [resp2::Data::BulkString { data: "GET", .. }, resp2::Data::BulkString { data: key, .. }] => {
+            [resp2::Data::BulkString("GET"), resp2::Data::BulkString(key)] => {
                 Ok((remaining, Command::Get(key)))
             }
-            [resp2::Data::BulkString { data: "SET", .. }, resp2::Data::BulkString { data: key, .. }, resp2::Data::BulkString { data: value, .. }] => {
+            [resp2::Data::BulkString("SET"), resp2::Data::BulkString(key), resp2::Data::BulkString(value)] => {
                 Ok((remaining, Command::Set(key, value)))
             }
-            [resp2::Data::BulkString { data: "KEYS", .. }, resp2::Data::BulkString { data: pattern, .. }] => {
+            [resp2::Data::BulkString("KEYS"), resp2::Data::BulkString(pattern)] => {
                 Ok((remaining, Command::Keys(pattern)))
             }
-            [resp2::Data::BulkString { data: "PING", .. }] => Ok((remaining, Command::Ping)),
-            [resp2::Data::BulkString { data: "QUIT", .. }] => Ok((remaining, Command::Quit)),
+            [resp2::Data::BulkString("PING")] => Ok((remaining, Command::Ping)),
+            [resp2::Data::BulkString("QUIT")] => Ok((remaining, Command::Quit)),
             _ => {
                 debug!("Failed to parse command: {:?}", arr);
                 Err(nom::Err::Error(nom::error::Error::new(
